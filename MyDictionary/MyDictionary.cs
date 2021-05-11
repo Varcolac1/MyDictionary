2337e22e59d81c57ac401080886814abdce1f700
Alper <mhmtalpr54@gmail.com>
2021-05-11 05:17:04 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<K, T>
+    {
+        K[] array;
+        K[] tempArray;
+        T[] array2;
+        T[] tempArray2;
+        public MyDictionary()
+        {
+            array = new K[0];
+            array2 = new T[0];
+        }
+        public void Add(K key, T type)
+        {
+            tempArray = array;
+            tempArray2 = array2;
+            array = new K[array.Length + 1];
+            array2 = new T[array2.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                array2[i] = tempArray2[i];
+            }
+
+            array[array.Length - 1] = key;
+            array2[array2.Length - 1] = type;
+
+        }
+        public int Length
+        {
+            get { return array.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> User = new MyDictionary<string, int>();
+            User.Add("Alper", 18);
+            User.Add("Ülkü", 18);
+            User.Add("Murat", 18);
+
+            /*foreach (var item in User) bu fonksiyonu kullanarak çıktı alacağım sonraya bırakıyorum çünkü ben bir eziğim çözemedim nasıl yapılacağını ehe
+            {
+
+            }*/
+            Console.WriteLine(User.Length);
+        }
+    }
+}
